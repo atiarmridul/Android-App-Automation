1ca02f6ba01bbe5dfa424d478cb6509bb5470a42
atiarmridul <atiarmridul@gmail.com>
2024-09-12 17:51:39 +0600

    new test case added

--- test/specs/android-findElements.spec.js
+++ test/specs/android-findElements.spec.js
@@ -2,25 +2,69 @@ describe('Android Elements Test', () => {
   
   it('Find element by accessibility id', async () => {
     // find element by accessibility id
-    const appOption = await $('~App');
+    const appOption = await $('~Views');
 
     // click on element
     await appOption.click();
 
     // assertion
-    const actionBar = await $('~Activity');
+    const actionBar = await $('~Animation');
     await expect(actionBar).toBeExisting();
     console.log('Assertion Successful');
     
   })
   
-  it.only('Find element by class name', async () => {
+  it('Find element by class name', async () => {
     // find element by class name
     const className = await $('android.widget.TextView');
     console.log(await className.getText());
     await expect(className).toHaveText("API Demos");
     
   })
+  
+
+  it('Find element by Xpath', async () => {
+
+      // find element by Xpath
+    await $('//android.widget.TextView[@content-desc="Alert Dialogs"]').click();
+      // find element by Resource ID
+    await $('//android.widget.Button[@resource-id="io.appium.android.apis:id/select_button"]').click();
+      // find element by Text
+    await $('//android.widget.TextView[@text="Command two"]').click();
+      // find element by class name
+    const testAssertion = await $('android.widget.TextView');
+      // assertion
+    await expect(testAssertion).toHaveText("You selected: 1 , Command two");
+
+    
+  })
+  
+  it('Find element by UiAutomator', async () => {
+    // find element by UiAutomator
+    await $('android=new UiSelector().textContains("Alert")').click();  
+  })
+
+
+
+  it.only('Find multiple elements', async () => {
+    
+    const expectedList = ['API Demos',"Access'ibility",'Accessibility','Animation','App',
+      'Content','Graphics','Media','NFC','OS','Preference','Text','Views'];
+    const actualList = []
+    const textList = await $$("android.widget.TextView")
+
+    for (const element of textList){
+      actualList.push(await element.getText())
+    }
+    await expect(actualList).toEqual(expectedList);
+
+    
+  })
+
+
+
+
+
 
 
 });
