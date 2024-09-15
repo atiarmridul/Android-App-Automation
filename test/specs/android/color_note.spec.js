92c674f3280ecbbacd3d4320d3e0386f179b55f2
atiarmridul <atiarmridul@gmail.com>
2024-09-16 01:29:37 +0600

    Page Object Model Implemented.

--- test/specs/android/color_note.spec.js
+++ test/specs/android/color_note.spec.js
@@ -1,64 +1,42 @@
+import  addNoteScreen  from "../../screenobjects/android/add-note.screen.js";
+
 describe('Working on the colornote application. ', () => {
   
     it('Skipping the tutorial', async () => {
        //skipping the tutorial
-       driver.pause(3000); 
-       (await $('//*[@text="SKIP"]')).click();
+        await addNoteScreen.skipButton.click();
 
     })
 
     it('Creating a note', async () => {
-       
-        //clicling the addnote button 
-        (await $('//*[@text="Add note"]')).click();
-        
-        //pause for 3sec
-        await driver.pause(3000); 
-        (await $('//*[@text="Text"]')).click();
-        
-        //pause for 3sec
-        await driver.pause(3000);
-        (await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_title"]')).addValue("About MySelf");
-        
-        //pause for 3sec
-        await driver.pause(3000);
-        (await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/edit_note"]')).addValue("My Name is Md. Atiar Rahman Chowdhury");
-        
-        //pause for 3sec
-        await driver.pause(3000);
-        
-        //clicking 2 times to save the note. 
+        //clicling the  button 
+        await addNoteScreen.addNote.click();  
+        //selecting text type
+        await addNoteScreen.selectText.click();  
+        //set the note title 
+        await addNoteScreen.titleField.addValue("About MySelf"); 
+        //set the note description
+        await addNoteScreen.descriptionField.addValue("My Name is Md. Atiar Rahman Chowdhury");  
+        //save the note.and 1 time to return to main activity 
+        await addNoteScreen.saveNote();
+        // 1 time to return to main activity 
         await driver.back();
-        await driver.back();
-        await driver.back();
-
         //Validating that note is created.
-        const createNote = await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/background"]');
-        await expect(createNote).toExist();
+        await expect(addNoteScreen.createNote).toExist();
      })
 
      it('Deleting a note', async () => {
-
-        
         //clicking the created note. 
-        (await $('//*[@resource-id="com.socialnmobile.dictapps.notepad.color.note:id/background"]')).click();
-        //pause for 3sec
-        await driver.pause(3000);
+        await addNoteScreen.createNote.click();
         //clicking the more (3 dot) menu
-        (await $('~More')).click();
-        //pause for 3sec
-        await driver.pause(3000);
+        await addNoteScreen.menuItem.click();
         //clicking the delete option
-        (await $('//*[@text="Delete"]')).click();
+        await addNoteScreen.deleteOption.click();
+        //confirming the delete
         await driver.acceptAlert();
-        //pause for 3sec
-        await driver.pause(3000);
-
         //Validating that note is deleted.
-        await expect(await $('//*[@text="Add note"]')).toExist();
+        await expect(addNoteScreen.addNote).toExist();
        
-
     })
-
 
 })
